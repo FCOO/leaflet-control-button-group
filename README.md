1793e89363998899f59220ce7551901792abcfed
NielsHolt <niels@steenbuchholt.dk>
2016-06-05 22:30:20 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # leaflet-control-button-group
-Leaflet control with multible buttons able of disabled, selected and unselected
+>
+
+
+## Description
+Create a Leaflet-control with one or more Fontawesome-icon buttons with (optional) text.
+It is possible to create selected/unselected buttons and to enable/disable the buttons 
+
+## Installation
+### bower
+`bower install https://github.com/FCOO/leaflet-control-button-group.git --save`
+
+## Demo
+http://FCOO.github.io/leaflet-control-button-group/demo/ 
+
+## Usage
+```var myButtonGroup = new L.Control.ButtonGroup( options );```
+
+
+### options
+
+| Id | Type | Default | Description |
+| :--: | :--: | :-----: | --- |
+| position | string | 'topleft' | Leaflet control position | 
+| horizontal | boolean | false | If true the buttons are horizontal aligned |
+| className | string | '' | Extra class-name(s) for the control-container |
+| buttons | array | [] | array of `buttonOptions` |
+
+### buttonOptions
+
+| Id | Type | Default | Description |
+| :--: | :--: | :-----: | --- |
+id | string | '' | The id of the button |
+icon | string | 'help' | The fontawesome icon-name without leading `fa-` |
+text | string | '' | The text next to the icon |
+disabled | boolean | false | If `true` the button is disabled |
+selectable | boolean | false | If `true` the button is selectable |
+selected | boolean | false | If `true` the button is selected AND selectable |
+selectedIcon | string | '' | The fontawesome icon-name to be used when the button is selected  |
+separatorBefore | boolean | false | Only if `options.horizontal = false`: Adds a horizontal separator before the button |
+hoverColor | string | null | Color for the icon, when the mouse is over the button and when the button is selected |
+title | string | '' | The `title`-attribute of the button |
+href | string | '' | If the button should link to another page  |
+onClick | function | null | function( button, map, selected ) called when a button is clicked |
+context | object | null | The context of the `onClick`-function (`context` will be refferent to by `this` inside `onClick`) |
+
+### Methods
+
+A button can be identified either by its index in the `options.buttons` array or by the (optional) id in its `buttonOptions`
+
+		.enableButton( indexOrId )
+		.disableButton( indexOrId )
+		.selectButton( indexOrId )
+		.unselectButton( indexOrId )
+
+## Copyright and License
+This plugin is licensed under the [MIT license](https://github.com/FCOO/leaflet-control-button-group/LICENSE).
+
+Copyright (c) 2016 [FCOO](https://github.com/FCOO)
+
+## Contact information
+
+[Niels Holt](http://github.com/NielsHolt)
---
+++ src/leaflet-control-button-group.js
@@ -0,0 +1,199 @@
+/****************************************************************************
+	leaflet-control-button-group.js,
+
+	(c) 2016, FCOO
+
+	https://github.com/FCOO/leaflet-control-button-group
+	https://github.com/FCOO
+
+****************************************************************************/
+;(function ($, L, window, document, undefined) {
+	"use strict";
+
+	var defaultButtonOptions = {
+		id							: '',
+		icon						: 'help',
+		text						: '',
+		disabled				: false,
+		selectable			: false,
+		selected				: false,
+		selectedIcon		: '',
+		separatorBefore	: false,
+		hoverColor			: null,
+		title						: '',
+		href						: '',
+		onClick					: function(){}, //function( button, map, selected );
+		context					: null
+	};
+
+
+
+	//Extend L.Control
+	L.Control.ButtonGroup = L.Control.extend({
+
+
+    //Default options
+		options: {
+			VERSION		: "{VERSION}",
+      position	: 'topleft',
+			horizontal: false,
+			buttons		: [],
+			className	: ''
+		},
+
+		buttons: [],	//List of buttons as html-element - both bottons[index] and button8id]
+
+		//initialize
+		initialize: function(options) {
+			L.setOptions(this, options);
+		},
+
+		onAdd: function (map) {
+			var i;
+      this._map = map;
+			this._container = this.createContainer();
+      L.DomEvent.disableClickPropagation( this._container );
+
+			for (i=0; i<this.options.buttons.length; i++ )
+				this.addButton( this.options.buttons[i] );
+
+			return this._container;
+    },
+
+		//createContainer
+		createContainer: function(){
+			return L.DomUtil.create('div', 'leaflet-bar '+ (this.options.horizontal ? 'horizontal ' : '') + this.options.className);
+		},
+
+		//addButton
+		addButton: function( options ){
+			var $button = this.$createButton( options, this._container ),
+					button = $button[0];
+			this.buttons.push( button );
+			if (options.id)
+				this.buttons[ options.id ] = button;
+		},
+
+		//$createButton
+		$createButton: function (options, container) {
+			options = L.extend({}, defaultButtonOptions, options);
+
+			//Set options.selectable if a selected-icon or selected is in options
+			options.selectable = options.selectable || !!options.selectedIcon || !!options.selected;
+
+			//Create the onClick-function
+			options.onClick = $.proxy( options.onClick, options.context );
+			var $i,
+					$link = $('<a>')
+									.addClass( (options.disabled ? ' leaflet-disabled' : '') )
+									.data('button', options);
+
+			if (options.text){
+				$link.addClass('text');
+
+				if (options.icon){
+					$i = $('<i>')
+						.addClass('fa fa-lg fa-'+options.icon)
+						.appendTo( $link );
+
+					if (options.hoverColor)
+						$i.css('color', options.hoverColor);
+				}
+				$link.append( options.text );
+			}
+			else {
+				$link.addClass('fa fa-lg fa-'+options.icon);
+				if (options.hoverColor)
+					$link.css('color', options.hoverColor);
+			}
+
+
+			if (options.href)
+				$link.attr('href', options.href);
+
+			if (options.title)
+				$link.attr('title', options.title);
+
+			$link.on( 'click', $.proxy( this._onClick, this ) );
+
+
+			//Add <hr> before button (only vertical)
+			if (options.separatorBefore && container)
+			  $('<hr>').appendTo( $(container) );
+
+			if (container)
+				$(container).append( $link );
+
+			if (options.selected)
+				this._selectButton( $link[0], true );
+
+			return $link;
+    },
+
+		//_onClick
+		_onClick: function( e ){
+			var button = e.currentTarget,
+					$button = $(button),
+					options = $button.data('button');
+			if (options.selectable)
+				this._selectButton( button, !$button.hasClass('selected') );
+			else {
+				options.onClick( button, this._map, null );
+			}
+		},
+
+
+		//_buttonToggleClass
+		_buttonToggleClass: function( button, className, state ){
+			if (button)
+				$(button).toggleClass( className, !!state );
+		},
+
+		//getButton
+		getButton: function( indexOrId ){ return this.buttons[ indexOrId ]; },
+
+		_enableButton: function( button, enabled ){ this._buttonToggleClass( button, 'leaflet-disabled', !enabled );	},
+
+		//enableButton
+		enableButton: function( indexOrId ){ this._enableButton(	this.getButton( indexOrId ), true  ); },
+
+		//disableButton
+		disableButton: function( indexOrId ){ this._enableButton( this.getButton( indexOrId ), false ); },
+
+
+		//_selectButton
+		_selectButton: function( button, selected ){
+			var $button = $(button),
+					options;
+			if (button){
+				this._buttonToggleClass( button, 'selected', selected );
+				options = $button.data('button');
+					if (options.selectedIcon){
+						this._buttonToggleClass( button, 'fa-' + options.icon,				 !selected );
+						this._buttonToggleClass( button, 'fa-' + options.selectedIcon,  selected );
+					}
+				options.onClick( button, this._map, selected );
+			}
+		},
+
+		//selectButton
+		selectButton: function( indexOrId )		{ this._selectButton( this.getButton( indexOrId ), true		); },
+
+		//unselectButton
+		unselectButton: function( indexOrId )	{ this._selectButton( this.getButton( indexOrId ), false	); },
+
+
+
+	});
+
+
+  L.control.buttonGroup = function (options) {
+    return new L.Control.ButtonGroup(options);
+  };
+
+	return L.Control.ButtonGroup;
+
+}(jQuery, L, this, document));
+
+
+
